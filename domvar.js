557f7ac27c0066e53644b70a2bd56a33d3dd9c71
Huỳnh Trọng Phúc <91412382+DEV-trongphuc@users.noreply.github.com>
2025-02-03 16:42:20 +0700

    Update domvar.js

--- domvar.js
+++ domvar.js
@@ -9,11 +9,17 @@ const quick_filter = [
 const goalMapping = {
   "Lead Form": ["LEAD_GENERATION", "QUALITY_LEAD"],
   Message: ["REPLIES"],
-  Awareness: ["REACH"],
-  Engagement: ["POST_ENGAGEMENT", "THRUPLAY"],
-  Traffic: ["PAGE_LIKES"],
-  Pagelike: ["OFFSITE_CONVERSIONS", "LINK_CLICKS", "PROFILE_VISIT"],
+  Awareness: ["REACH", "AD_RECALL_LIFT", "IMPRESSIONS"],
+  Engagement: ["POST_ENGAGEMENT", "THRUPLAY", "EVENT_RESPONSES"],
+  Traffic: [
+    "OFFSITE_CONVERSIONS",
+    "LINK_CLICKS",
+    "PROFILE_VISIT",
+    "LADING_PAGE_VIEWS",
+  ],
+  Pagelike: ["PAGE_LIKES"],
 };
+
 
 const accessToken =
   "EAAc7gNsIj8cBO2pWLpt97kgsyz8asflZA7Smwivuvxi8F203YWDznicZCHghqWUK9G6MKy1EYZBEXxY8zbdHhuGIhp8IwBveusxlJOIxqZAE28DVnfmLl3ejSbZBpxV8dvVJrC6aSAbOpu9oyZAvA64yxXOSu5sYp2EPXBljEHNHfqZB6F1usnQq70zCtZAI3ong"; // Token của bạn
