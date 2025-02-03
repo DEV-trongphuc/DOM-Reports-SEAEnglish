79d1b770a986cca8cf102c7ae766c798aa4b80af
Huỳnh Trọng Phúc <91412382+DEV-trongphuc@users.noreply.github.com>
2025-02-03 15:21:01 +0700

    Create domvar.js

---
+++ domvar.js
@@ -0,0 +1,20 @@
+const quick_filter = [
+  "Lead Form",
+  "Message",
+  "Awareness",
+  "Engagement",
+  "Traffic",
+  "Pagelike",
+];
+const goalMapping = {
+  "Lead Form": ["LEAD_GENERATION", "QUALITY_LEAD"],
+  Message: ["REPLIES"],
+  Awareness: ["REACH"],
+  Engagement: ["POST_ENGAGEMENT", "THRUPLAY"],
+  Traffic: ["PAGE_LIKES"],
+  Pagelike: ["OFFSITE_CONVERSIONS", "LINK_CLICKS", "PROFILE_VISIT"],
+};
+
+const accessToken =
+  "EAAc7gNsIj8cBO2pWLpt97kgsyz8asflZA7Smwivuvxi8F203YWDznicZCHghqWUK9G6MKy1EYZBEXxY8zbdHhuGIhp8IwBveusxlJOIxqZAE28DVnfmLl3ejSbZBpxV8dvVJrC6aSAbOpu9oyZAvA64yxXOSu5sYp2EPXBljEHNHfqZB6F1usnQq70zCtZAI3ong"; // Token của bạn
+const adAccountId = "1666500787177995"; // ID tài khoản quảng cáo
