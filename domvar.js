5b7157cfef01a351b4101fa7861101a37fb2327d
Huỳnh Trọng Phúc <91412382+DEV-trongphuc@users.noreply.github.com>
2025-02-10 11:37:33 +0700

    Update domvar.js

--- domvar.js
+++ domvar.js
@@ -24,3 +24,4 @@ const goalMapping = {
 const accessToken =
   "EAAc7gNsIj8cBO2pWLpt97kgsyz8asflZA7Smwivuvxi8F203YWDznicZCHghqWUK9G6MKy1EYZBEXxY8zbdHhuGIhp8IwBveusxlJOIxqZAE28DVnfmLl3ejSbZBpxV8dvVJrC6aSAbOpu9oyZAvA64yxXOSu5sYp2EPXBljEHNHfqZB6F1usnQq70zCtZAI3ong"; // Token của bạn
 const adAccountId = "1666500787177995"; // ID tài khoản quảng cáo
+let oodo_view = false;
