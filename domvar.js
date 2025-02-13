3ed7f05120ca33788cce294254bbf30600823542
Huỳnh Trọng Phúc <91412382+DEV-trongphuc@users.noreply.github.com>
2025-02-13 20:40:31 +0700

    Update domvar.js

--- domvar.js
+++ domvar.js
@@ -25,3 +25,13 @@ const accessToken =
   "EAAc7gNsIj8cBO2pWLpt97kgsyz8asflZA7Smwivuvxi8F203YWDznicZCHghqWUK9G6MKy1EYZBEXxY8zbdHhuGIhp8IwBveusxlJOIxqZAE28DVnfmLl3ejSbZBpxV8dvVJrC6aSAbOpu9oyZAvA64yxXOSu5sYp2EPXBljEHNHfqZB6F1usnQq70zCtZAI3ong"; // Token của bạn
 const adAccountId = "1666500787177995"; // ID tài khoản quảng cáo
 let oodo_view = false;
+const NAME = "SEA English";
+const LOGO_ACCOUNT = "./DOM-img/Logo.png";
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#dom_account_name").textContent = NAME;
+  document.querySelector("#dom_account_name_r").textContent = NAME;
+  document.querySelector("#dom_account_id").textContent = adAccountId;
+  document.querySelector("#dom_account_src").src = LOGO_ACCOUNT;
+  document.querySelector("#dom_account_src_r").src = LOGO_ACCOUNT;
+  document.title = `${NAME} - Meta Ads Report by DOM Marketing`;
+});
