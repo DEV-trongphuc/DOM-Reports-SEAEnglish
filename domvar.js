e04ca44b9ed2cea235f7bcccd1e1ababb63efa2b
Huỳnh Trọng Phúc <91412382+DEV-trongphuc@users.noreply.github.com>
2025-02-21 11:05:00 +0700

    Update domvar.js

--- domvar.js
+++ domvar.js
@@ -1,37 +1,12 @@
-const quick_filter = [
+const quickFilter = [
   "Lead Form",
-  "Message",
   "Awareness",
   "Engagement",
+  "Message",
   "Traffic",
   "Pagelike",
 ];
-const goalMapping = {
-  "Lead Form": ["LEAD_GENERATION", "QUALITY_LEAD"],
-  Message: ["REPLIES"],
-  Awareness: ["REACH", "AD_RECALL_LIFT", "IMPRESSIONS"],
-  Engagement: ["POST_ENGAGEMENT", "THRUPLAY", "EVENT_RESPONSES"],
-  Traffic: [
-    "OFFSITE_CONVERSIONS",
-    "LINK_CLICKS",
-    "PROFILE_VISIT",
-    "LADING_PAGE_VIEWS",
-  ],
-  Pagelike: ["PAGE_LIKES"],
-};
-
-
-const accessToken =
-  "EAAc7gNsIj8cBO2pWLpt97kgsyz8asflZA7Smwivuvxi8F203YWDznicZCHghqWUK9G6MKy1EYZBEXxY8zbdHhuGIhp8IwBveusxlJOIxqZAE28DVnfmLl3ejSbZBpxV8dvVJrC6aSAbOpu9oyZAvA64yxXOSu5sYp2EPXBljEHNHfqZB6F1usnQq70zCtZAI3ong"; // Token của bạn
-const adAccountId = "1666500787177995"; // ID tài khoản quảng cáo
-let oodo_view = false;
-const NAME = "SEA English";
-const LOGO_ACCOUNT = "./DOM-img/Logo.png";
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("#dom_account_name").textContent = NAME;
-  document.querySelector("#dom_account_name_r").textContent = NAME;
-  document.querySelector("#dom_account_id").textContent = adAccountId;
-  document.querySelector("#dom_account_src").src = LOGO_ACCOUNT;
-  document.querySelector("#dom_account_src_r").src = LOGO_ACCOUNT;
-  document.title = `${NAME} - Meta Ads Report by DOM Marketing`;
-});
+let isBrand = false;
+const accName = "SEA English";
+const accAvatar = "./DOM-img/Logo.png";
+const adAccountId = "1666500787177995"; 
