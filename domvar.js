a2d59194978df0cdfc8226844b9ad1001b6877e1
Huỳnh Trọng Phúc <91412382+DEV-trongphuc@users.noreply.github.com>
2025-02-21 11:18:14 +0700

    Update domvar.js

--- domvar.js
+++ domvar.js
@@ -1,3 +1,6 @@
+const accessToken =
+  "EAAc7gNsIj8cBO2pWLpt97kgsyz8asflZA7Smwivuvxi8F203YWDznicZCHghqWUK9G6MKy1EYZBEXxY8zbdHhuGIhp8IwBveusxlJOIxqZAE28DVnfmLl3ejSbZBpxV8dvVJrC6aSAbOpu9oyZAvA64yxXOSu5sYp2EPXBljEHNHfqZB6F1usnQq70zCtZAI3ong"; // Token của bạn
+
 const quickFilter = [
   "Lead Form",
   "Awareness",
