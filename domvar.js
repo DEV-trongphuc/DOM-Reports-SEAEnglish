766a7d57df800b8b35d5835ed2a8e4c801c8e390
Huỳnh Trọng Phúc <91412382+DEV-trongphuc@users.noreply.github.com>
2025-03-02 17:23:24 +0700

    Update domvar.js

--- domvar.js
+++ domvar.js
@@ -1,15 +1,18 @@
-const accessToken =
-  "EAAc7gNsIj8cBO2pWLpt97kgsyz8asflZA7Smwivuvxi8F203YWDznicZCHghqWUK9G6MKy1EYZBEXxY8zbdHhuGIhp8IwBveusxlJOIxqZAE28DVnfmLl3ejSbZBpxV8dvVJrC6aSAbOpu9oyZAvA64yxXOSu5sYp2EPXBljEHNHfqZB6F1usnQq70zCtZAI3ong"; // Token của bạn
-
-const quickFilter = [
-  "Lead Form",
-  "Awareness",
-  "Engagement",
-  "Message",
-  "Traffic",
-  "Pagelike",
+let listAccount = [
+  {
+    brand: false,
+    quick: [
+      "Lead Form",
+      "Message",
+      "Engagement",
+      "Traffic",
+      "Pagelike",
+      "Awareness",
+    ],
+    avatar: "./DOM-img/Logo.png",
+    name: "SEA English",
+    id: "1666500787177995",
+    access:
+      "EAAc7gNsIj8cBO2pWLpt97kgsyz8asflZA7Smwivuvxi8F203YWDznicZCHghqWUK9G6MKy1EYZBEXxY8zbdHhuGIhp8IwBveusxlJOIxqZAE28DVnfmLl3ejSbZBpxV8dvVJrC6aSAbOpu9oyZAvA64yxXOSu5sYp2EPXBljEHNHfqZB6F1usnQq70zCtZAI3ong",
+  },
 ];
-let isBrand = false;
-const accName = "SEA English";
-const accAvatar = "./DOM-img/Logo.png";
-const adAccountId = "1666500787177995"; 
